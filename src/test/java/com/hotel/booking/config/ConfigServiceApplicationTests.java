be253c0bde59485acf0aa356a985e5b5432d0358
mowumib <mowumi.odu@gmail.com>
2025-04-09 19:23:15 +0100

    feat: config service

---
+++ src/test/java/com/hotel/booking/config/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.booking.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
